0cfb46dd664de5a0686935cc4f53a4148c26c5d4
cjdreamy <cjdreamy402@gmail.com>
2026-08-17 01:03:33 +0300

    add

---
+++ add.py
@@ -0,0 +1,13 @@
+class Add:
+ def __init__(self):
+  self.a
+  self.b
+ def Take(self):
+  a = input("enter 1st num: ")
+  b = input("enter 2nd num: ")
+ def output(self):
+  print(a+b)
+
+nums = Add()
+nums.Take()
+nums.output()
